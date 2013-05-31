e5cb493b1bd9b142285d5ce662278290dac5019b
MatmaRex <matma.rex@gmail.com>
2013-05-31 23:17:05 +0200

    +multiwiki-scanner for finding non-default messages on WMF wikis

---
+++ multiwiki-scanner.rb
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+require 'sunflower'
+
+meta = Sunflower.new 'meta'
+sites = meta.API('action=sitematrix&smlimit=max')['sitematrix']
+sites.delete 'count'
+sites = sites.delete('specials') + sites.values.map{|a| a['site'] }.inject(:+)
+
+# sites = [{'url' => 'w:pl'}]
+
+sites.each do |hash|
+	next if hash['closed'] || hash['private'] || hash['fishbowl']
+	#next if hash['dbname'].start_with? 'wikimania'
+	
+	print "#{hash['url'].sub %r|^https?://|, ''}: "
+	
+	s = Sunflower.new hash['url']
+	p = s.page 'MediaWiki:mainpage'
+	hascustom = p.text != ''
+	
+	messagecontents = s.API('action=query&meta=allmessages&ammessages=mainpage')
+	messagecontents = messagecontents['query']['allmessages'][0]['*']
+	
+	puts hascustom ? "CUSTOM #{messagecontents}" : 'DEFAULT'
+end
